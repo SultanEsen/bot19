fe3e761503a679d0f032cf9f41804f3ec339d21d
SultanEsen <beksultan.esenkulov@gmail.com>
2022-08-01 22:39:55 +0600

    inline added 3

--- handlers/inline.py
+++ handlers/inline.py
@@ -39,7 +39,23 @@ async def inline_wiki_handler(query: types.InlineQuery):
     await query.answer(article, cache_time=60, is_personal=True)
 
 
+async def inline_google_handler(query: types.InlineQuery):
+    text = query.query or 'echo'
+    link = f'https://www.google.com/search?q={text}'
+    result_id: str = hashlib.md5(text.encode()).hexdigest()
+    article = [types.InlineQueryResultArticle(
+        id=result_id,
+        title='Google',
+        url=link,
+        input_message_content=types.InputMessageContent(
+            message_text=link
+        )
+    )]
+    await query.answer(article, cache_time=60, is_personal=True)
+
+
 def register_inline_handler(dp: Dispatcher):
     # dp.register_inline_handler(inline_youtube_handler)
-    dp.register_inline_handler(inline_wiki_handler)
+    # dp.register_inline_handler(inline_wiki_handler)
+    dp.register_inline_handler(inline_google_handler)
 
