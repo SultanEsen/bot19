efeb2cff860ecf662bb7f9e573bce3403d520bea
SultanEsen <beksultan.esenkulov@gmail.com>
2022-08-01 22:33:06 +0600

    inline added 2

--- handlers/inline.py
+++ handlers/inline.py
@@ -24,6 +24,22 @@ async def inline_youtube_handler(query: types.InlineQuery):
     await query.answer(articles, cache_time=60, is_personal=True)
 
 
+async def inline_wiki_handler(query: types.InlineQuery):
+    text = query.query or 'echo'
+    link = f'https://ru.wikipedia.org/wiki/{text}'
+    result_id: str = hashlib.md5(text.encode()).hexdigest()
+    article = [types.InlineQueryResultArticle(
+        id=result_id,
+        title='Wiki',
+        url=link,
+        input_message_content=types.InputMessageContent(
+            message_text=link
+        )
+    )]
+    await query.answer(article, cache_time=60, is_personal=True)
+
+
 def register_inline_handler(dp: Dispatcher):
-    dp.register_inline_handler(inline_youtube_handler)
+    # dp.register_inline_handler(inline_youtube_handler)
+    dp.register_inline_handler(inline_wiki_handler)
 
